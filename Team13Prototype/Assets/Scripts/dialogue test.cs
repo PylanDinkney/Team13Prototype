00b2883cc335e57559772dd40f76135f8449a914
wksnders <42388072+wksnders@users.noreply.github.com>
2021-09-10 18:51:41 -0600

    updated dialog tests with player options
    added conversation options for the player in dialogue test
    added display choices to create a list of options for the player to say when in dialogue

--- Team13Prototype/Assets/Scripts/dialogue test.cs
+++ Team13Prototype/Assets/Scripts/dialogue test.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 
 public struct Conversation
 {
-    public Dictionary<string, string> conversation; //formatted keys : "12" where 1 would represent choosing the first option in the first choice, 2 the second option in the second choice and so on. 
+    public Dictionary<string, string> responses; //formatted keys : "12" where 1 would represent choosing the first option in the first choice, 2 the second option in the second choice and so on. 
     //fomatted values : string that contains what the npc response is to the choices
-    public Dictionary<int, int> choices; //key : what choice are we on starting at 1 being the first choice, a choice of how the player greets this npc
+    public Dictionary<string, string> choices; //formatted keys : "12" where 1 would represent choosing the first option in the first choice, 2 the second option in the second choice and so on. 
+    //fomatted values : string that contains what the player says 
+    public Dictionary<int, int> numChoices; //key : what choice are we on starting at 1 being the first choice, a choice of how the player greets this npc
     //value : how many choices for this
 }
 
@@ -26,35 +28,53 @@ public class dialoguetest : MonoBehaviour
         //add choices to the conversation 
         //find how many choices are in each layer of conversation
         // find all keys, sort by length to find the amt of choices for each layer
-        conversation1.choices = new Dictionary<int, int>();
-        conversation1.conversation = new Dictionary<string, string>();
+        conversation1.numChoices = new Dictionary<int, int>();
+        conversation1.responses = new Dictionary<string, string>();
 
         //---conversation 1
-        conversation1.choices[1] = 2;
-        conversation1.conversation.Add("1", "this is my reponse to the player choosing the first option");
-        conversation1.conversation.Add("2", "this is my reponse to the player choosing the second option");
+        conversation1.numChoices[1] = 2;
+        conversation1.choices.Add("1", "this is the first option the player can choose to say");
+        conversation1.choices.Add("2", "this is the second option the player can choose to say");
+        conversation1.responses.Add("1", "this is my reponse to the player choosing the first option");
+        conversation1.responses.Add("2", "this is my reponse to the player choosing the second option");
 
-        conversation1.choices[2] = 4;
-        conversation1.conversation.Add("11", "this is my reponse to the player choosing the first option then the first option");
-        conversation1.conversation.Add("12", "this is my reponse to the player choosing the second option then the second option");
-        conversation1.conversation.Add("13", "this is my reponse to the player choosing the first option then the third option");
-        conversation1.conversation.Add("14", "this is my reponse to the player choosing the second option then the fourth option");
+        conversation1.numChoices[2] = 4;
+        conversation1.choices.Add("11", "this is the first option");
+        conversation1.choices.Add("12", "this is the second option");
+        conversation1.choices.Add("13", "this is the third option");
+        conversation1.choices.Add("14", "this is the fourth option");
+        conversation1.responses.Add("11", "this is my reponse to the player choosing the first option then the first option");
+        conversation1.responses.Add("12", "this is my reponse to the player choosing the second option then the second option");
+        conversation1.responses.Add("13", "this is my reponse to the player choosing the first option then the third option");
+        conversation1.responses.Add("14", "this is my reponse to the player choosing the second option then the fourth option");
 
-        conversation1.conversation.Add("21", "this is my reponse to the player choosing the first option then the first option");
-        conversation1.conversation.Add("22", "this is my reponse to the player choosing the second option then the second option");
-        conversation1.conversation.Add("23", "this is my reponse to the player choosing the first option then the third option");
-        conversation1.conversation.Add("24", "this is my reponse to the player choosing the second option then the fourth option");
+        conversation1.choices.Add("21", "this is the first option");
+        conversation1.choices.Add("22", "this is the second option");
+        conversation1.choices.Add("23", "this is the third option");
+        conversation1.choices.Add("24", "this is the fourth option");
+        conversation1.responses.Add("21", "this is my reponse to the player choosing the first option then the first option");
+        conversation1.responses.Add("22", "this is my reponse to the player choosing the second option then the second option");
+        conversation1.responses.Add("23", "this is my reponse to the player choosing the first option then the third option");
+        conversation1.responses.Add("24", "this is my reponse to the player choosing the second option then the fourth option");
 
-        conversation1.choices[3] = 1;
-        conversation1.conversation.Add("111", "this is my reponse last response to the player");
-        conversation1.conversation.Add("121", "this is my reponse last response to the player");
-        conversation1.conversation.Add("131", "this is my reponse last response to the player");
-        conversation1.conversation.Add("141", "this is my reponse last response to the player");
+        conversation1.numChoices[3] = 1;
+        conversation1.choices.Add("111", "player's last message");
+        conversation1.choices.Add("121", "player's last message");
+        conversation1.choices.Add("131", "player's last message");
+        conversation1.choices.Add("141", "player's last message");
+        conversation1.responses.Add("111", "this is my reponse last response to the player");
+        conversation1.responses.Add("121", "this is my reponse last response to the player");
+        conversation1.responses.Add("131", "this is my reponse last response to the player");
+        conversation1.responses.Add("141", "this is my reponse last response to the player");
 
-        conversation1.conversation.Add("211", "this is my reponse last response to the player");
-        conversation1.conversation.Add("221", "this is my reponse last response to the player");
-        conversation1.conversation.Add("231", "this is my reponse last response to the player");
-        conversation1.conversation.Add("241", "this is my reponse last response to the player");
+        conversation1.choices.Add("211", "player's last message");
+        conversation1.choices.Add("221", "player's last message");
+        conversation1.choices.Add("231", "player's last message");
+        conversation1.choices.Add("241", "player's last message");
+        conversation1.responses.Add("211", "this is my reponse last response to the player");
+        conversation1.responses.Add("221", "this is my reponse last response to the player");
+        conversation1.responses.Add("231", "this is my reponse last response to the player");
+        conversation1.responses.Add("241", "this is my reponse last response to the player");
 
         conversations.Add("conversation 1", conversation1);
 
@@ -65,8 +85,22 @@ public class dialoguetest : MonoBehaviour
     {
         
     }
+    // choice number is the number of choices that the player has peviously made in this conversation +1
+    public void DisplayChoices(int choiceNumber) { //displays a list of playerchoices for what they can say in a conversation
+        int numOptions = conversations[currentConversation].numChoices[choiceNumber];
+        List<string> displaychoices = new List<string>();
+        for (int i = 1; i <= numOptions; i++)
+        {
+            string temp = conversationIndex + "" + i;//get all possible next conversation choices
+            displaychoices.Add(conversations[currentConversation].choices[temp]);//get the text for each choice and store it in display choices
+        }
 
-    public void RespondToPlayer(string playerChoice)// event handler? takes in what the player's last response was and gives the npc response
+        print(displaychoices);//TODO: display dialogue choices
+
+    }
+
+    // player choice is the number of the option that the player choose, starts at 1
+    public void RespondToPlayer(string playerChoice)// takes in what the player's last response was and gives the npc response
     {
         if (conversationIndex == "141")//this option makes the npc not respond to the player
         {
@@ -75,9 +109,9 @@ public class dialoguetest : MonoBehaviour
         }
 
         conversationIndex = conversationIndex + playerChoice; // append player's choice to the conversation pointer (remember previous choices)
-        string message = conversations[currentConversation].conversation[conversationIndex];//get the next message that the npc is going to say
+        string message = conversations[currentConversation].responses[conversationIndex];//get the next message that the npc is going to say
 
-        print(message); //TODO display dialogue for the modified conversation index
+        print(message); //TODO: display dialogue for the modified conversation index
 
         if (conversationIndex == "131") { //if the player gets the correct squence then another thing happens or they join your team
             //player chose option 131 
