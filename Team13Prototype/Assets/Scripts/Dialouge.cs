6bef7446ac791d734ceb3cb9dc89db4e834d3064
majohn17 <46938767+majohn17@users.noreply.github.com>
2021-09-10 18:52:13 -0600

    Edited the dialogue method and fixed the null refenrece error.

--- Team13Prototype/Assets/Scripts/Dialouge.cs
+++ Team13Prototype/Assets/Scripts/Dialouge.cs
@@ -6,52 +6,19 @@ using TMPro;
 public class Dialouge : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    public string[] lines;
+    public string currentText;
     public float textSpeed;
-    private int index;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void NextDialogue() {
         textComponent.text = string.Empty;
-        StartDialouge();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) {
-            if (textComponent.text == lines[index]) {
-                NextLine();
-            }
-            else {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
-
-    void StartDialouge() {
-        index = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
-        foreach (char c in lines[index].ToCharArray()) {
+        foreach (char c in currentText.ToCharArray()) {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine() {
-        if (index < lines.Length - 1) {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else {
-            gameObject.SetActive(false);
         }
     }
 }
