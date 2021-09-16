1dabf237d8b8d9e22546966f1376d802ce8fcdec
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-09-16 14:00:49 -0600

    Made picking up items work

--- Team13Prototype/Assets/Scripts/PickupObject.cs
+++ Team13Prototype/Assets/Scripts/PickupObject.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class PickupObject : MonoBehaviour
 {
     public string item = "emptyItem";
+    private bool inRange;
+
+    void Start(){
+        inRange = false;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.C) && inRange == true){
+            Debug.Log("Picked up item!");
+            GameObject player = GameObject.Find(GameConstants.Possessable[GameConstants.currentPossession]);
+            playerAttributes playerAttributes = player.gameObject.GetComponent<playerAttributes>();
+            playerAttributes.item = item;
+        } 
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == GameConstants.Possessable[GameConstants.currentPossession] && Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("Picked up item!");
-            playerAttributes playerAttributes = other.gameObject.GetComponent<playerAttributes>();
-            playerAttributes.item = item;
+        if(other.gameObject.name == GameConstants.Possessable[GameConstants.currentPossession]){
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.name == GameConstants.Possessable[GameConstants.currentPossession]){
+            inRange = false;
         }
     }
 }
