82a0bee71508f4b3fe9cad212604520fb97a3dfe
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-09-16 13:11:51 -0600

    Added Character Template Variables

---
+++ Team13Prototype/Assets/Scripts/playerAttributes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttributes : MonoBehaviour
+{
+    public string name;
+    public int conversionRate; 
+    public string type;
+    public string item;
+    public string itemWeakness;
+}
