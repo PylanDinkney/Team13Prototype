aacf369a0a9cde14c404587fc2a41bf71974fd40
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-09-17 12:23:27 -0600

    Added Q as a switch body backwards key

--- Team13Prototype/Assets/Scripts/SwitchBodies.cs
+++ Team13Prototype/Assets/Scripts/SwitchBodies.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class SwitchBodies : MonoBehaviour
 {
     private static bool keyIsDown;
+    private static bool eWentDown;
+    private static bool qWentDown;
     public string SwitchBodiesKey = "e";
+    public string SwitchBodiesKeyBack = "q";
 
     void Start(){
         keyIsDown = false;
+        eWentDown = false;
+        qWentDown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(SwitchBodiesKey) && keyIsDown == false)
+        if((Input.GetKeyDown(SwitchBodiesKey) || Input.GetKeyDown(SwitchBodiesKeyBack)) && keyIsDown == false)
         {
             keyIsDown = true;
             if(GameConstants.Possessable.Count >= 2)
             {
-                GameConstants.currentPossession++;
+                if(Input.GetKeyDown(SwitchBodiesKey)){
+                    GameConstants.currentPossession++;
+                    eWentDown = true;
+                }
+                else{
+                    GameConstants.currentPossession--;
+                    qWentDown = true;
+                }
                 if (GameConstants.currentPossession >= GameConstants.Possessable.Count) {
                     GameConstants.currentPossession = 0;
+                } else if(GameConstants.currentPossession < 0){
+                    GameConstants.currentPossession = GameConstants.Possessable.Count - 1;
                 }
             }
-        } else if(Input.GetKeyUp(SwitchBodiesKey) && keyIsDown == true){
+        } else if(Input.GetKeyUp(SwitchBodiesKey) && keyIsDown == true && eWentDown == true){
             keyIsDown = false;
+            eWentDown = false;
+        } else if(Input.GetKeyUp(SwitchBodiesKeyBack) && keyIsDown == true && qWentDown == true){
+            keyIsDown = false;
+            qWentDown = false;
         }
     }
 }
