066b8a84ae03bf87fc3e60414fa0d43a5370a556
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-09-26 10:52:11 -0600

    Fixed Audio Bugs

---
+++ Team13Prototype/Assets/Scripts/getKeySoundScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getKeySoundScript : MonoBehaviour
+{
+    public AudioSource getKeySound;
+    private bool playedSound;
+    playerAttributes tomAttrib;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tom = GameObject.Find("Tom");
+        tomAttrib = tom.GetComponent<playerAttributes>();
+        playedSound = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tomAttrib.Item == "Office Key" && playedSound == false){
+            getKeySound.Play();
+            playedSound = true;
+        }
+        else if(tomAttrib.Item != "Office Key" && playedSound == true){
+            playedSound = false;
+        }
+    }
+}
